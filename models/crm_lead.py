d9f86f16fe81eea6d5f2002588a201f592b98adf
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-04-24 11:07:59 +0530

    Add unread WhatsApp indicator to CRM leads and update Kanban view

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -4,3 +4,9 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     has_unread_whatsapp = fields.Boolean(string='Unread WhatsApp', default=False)
+    show_whatsapp_indicator = fields.Boolean(compute='_compute_show_whatsapp_indicator')
+
+    @api.depends('has_unread_whatsapp')
+    def _compute_show_whatsapp_indicator(self):
+        for record in self:
+            record.show_whatsapp_indicator = record.has_unread_whatsapp
